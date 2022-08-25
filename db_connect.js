05a4a6b3f118e1a6f75d31ee3b24d39240776924
huonguy <uthuong125@gmail.com>
2022-08-25 16:03:10 +0700

    update cofig

--- db_connect.js
+++ db_connect.js
@@ -1,12 +1,23 @@
 const { Sequelize } = require("sequelize");
 
+// const sequelize = new Sequelize(
+//   "db_User", //database name
+//   "root", //username
+//   "admin123", //password
+//   {
+//     host: "127.0.0.1", //url sql server, RDMS
+//     port: "3308", //port
+//     dialect: "mysql", //sql server dang su dung
+//   }
+// );
+
 const sequelize = new Sequelize(
-  "db_User", //database name
-  "root", //username
-  "admin123", //password
+  "heroku_51b5dca58ca36ed", //database name
+  "b6bc9bec7ff7d5", //username
+  "6244cff8", //password
   {
-    host: "127.0.0.1", //url sql server, RDMS
-    port: "3308", //port
+    host: "us-cdbr-east-06.cleardb.net", //url sql server, RDMS
+    port: "3306", //port
     dialect: "mysql", //sql server dang su dung
   }
 );
